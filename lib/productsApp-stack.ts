d63acafb1fe1d6454b1c2e25de245bde7daacd19
Thiago Mendes <thiago.mendes777@hotmail.com>
2024-01-16 19:21:22 -0300

    Criando a função de eventos de produtos[aula31]

--- lib/productsApp-stack.ts
+++ lib/productsApp-stack.ts
@@ -6,12 +6,15 @@ import * as ssm from "aws-cdk-lib/aws-ssm"
 
 import { Construct } from "constructs"
 
+interface ProductsAppStackProps extends cdk.StackProps {
+   eventsDdb: dynamodb.Table
+}
 export class ProductsAppStack extends cdk.Stack {
    readonly productsFetchHandler: lambdaNodeJS.NodejsFunction
    readonly productsAdminHandler: lambdaNodeJS.NodejsFunction
    readonly productsDdb: dynamodb.Table
 
-   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+   constructor(scope: Construct, id: string, props: ProductsAppStackProps) {
       super(scope, id, props)
 
       this.productsDdb = new dynamodb.Table(this, "ProductsDdb", {
@@ -28,6 +31,26 @@ export class ProductsAppStack extends cdk.Stack {
 
       const productsLayerArn = ssm.StringParameter.valueForStringParameter(this, "ProducstLayerVersionArn")
       const productsLayer = lambda.LayerVersion.fromLayerVersionArn(this, "ProducstLayerVersionArn", productsLayerArn)
+
+      const productEventsHandler = new lambdaNodeJS.NodejsFunction(this,
+         "ProductEventsFunction", {
+         functionName: "ProductEventsFunction",
+         entry: "lambda/products/ProductEventsFunction.ts",
+         handler: "handler",
+         memorySize: 128,
+         timeout: cdk.Duration.seconds(5),
+         bundling: {
+            minify: true,
+            sourceMap: false
+         },
+         environment: {
+            EVENTS_DDB: props.eventsDdb.tableName
+         },
+         tracing: lambda.Tracing.ACTIVE,
+         insightsVersion: lambda.LambdaInsightsVersion.VERSION_1_0_119_0
+      })
+
+      props.eventsDdb.grantWriteData(productEventsHandler)
 
       this.productsFetchHandler = new lambdaNodeJS.NodejsFunction(this,
          "ProductsFetchFunction", {
@@ -63,13 +86,15 @@ export class ProductsAppStack extends cdk.Stack {
             sourceMap: false
          },
          environment: {
-            PRODUCTS_DDB: this.productsDdb.tableName
+            PRODUCTS_DDB: this.productsDdb.tableName,
+            PRODUCT_EVENTS_FUNCTION_NAME: productEventsHandler.functionName
          },
          layers: [productsLayer],
          tracing: lambda.Tracing.ACTIVE,
          insightsVersion: lambda.LambdaInsightsVersion.VERSION_1_0_119_0
       })
       this.productsDdb.grantWriteData(this.productsAdminHandler)
+      productEventsHandler.grantInvoke(this.productsAdminHandler)
 
    }
 }
