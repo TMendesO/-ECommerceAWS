77450c28bf6eebf1165fac66705adaaa9bcd4ab8
Thiago Mendes <thiago.mendes777@hotmail.com>
2024-05-06 19:30:16 -0300

    Criando a função de pedidos[aula41]

--- lib/ordersApp-stack.ts
+++ lib/ordersApp-stack.ts
@@ -11,6 +11,9 @@ interface OrdersAppStackProps extends cdk.StackProps {
 }
 
 export class OrdesAppStack extends cdk.Stack {
+
+    readonly ordersHandler: lambdaNodeJS.NodejsFunction
+
     constructor(scope: Construct, id: string, props: OrdersAppStackProps) {
         super(scope, id, props)
 
@@ -40,6 +43,26 @@ export class OrdesAppStack extends cdk.Stack {
         const productsLayerArn = ssm.StringParameter.valueForStringParameter(this, "ProductsLayerVersionArn")
         const productsLayer = lambda.LayerVersion.fromLayerVersionArn(this, "ProductsLayerVersionArn", productsLayerArn)
 
+        this.ordersHandler = new lambdaNodeJS.NodejsFunction(this, "OrdersFunction", {
+            functionName: "OrdersFunction",
+            entry: "lambda/orders/ordersFunction.ts",
+            handler: "handler",
+            memorySize: 512,
+            timeout: cdk.Duration.seconds(5),
+            bundling: {
+                minify: true,
+                sourceMap: false
+            },
+            environment: {
+                PRODUCTS_DDB: props.productsDdb.tableName,
+                ORDERS_DDB: ordersDbd.tableName
+            },
+            layers: [/*ordersLayer,*/ productsLayer], //ordersLayer ainda tem que ser criada
+            tracing: lambda.Tracing.ACTIVE,
+            insightsVersion: lambda.LambdaInsightsVersion.VERSION_1_0_119_0
+        })
 
+        ordersDbd.grantReadWriteData(this.ordersHandler)
+        props.productsDdb.grantReadWriteData(this.ordersHandler)
     }
 }
